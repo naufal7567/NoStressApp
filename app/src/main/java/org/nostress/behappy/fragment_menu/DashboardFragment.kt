abc2a3b96051f1e45d10c39f5134b15d56f6713f
naufal7567 <naufal7567@gmail.com>
2021-06-01 04:13:23 +0700

    add psikolog fragment

--- app/src/main/java/org/nostress/behappy/fragment_menu/DashboardFragment.kt
+++ app/src/main/java/org/nostress/behappy/fragment_menu/DashboardFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 import org.nostress.behappy.R
 
 
@@ -14,9 +15,19 @@ class DashboardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        btn_gejala_stres.setOnClickListener { replaceFragment(PsikologFragment) }
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
-
+    private fun replaceFragment(fragment: Fragment){
+        if (fragment != null){
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.fragment_container, fragment)
+            transaction?.commit()
+        }
+    }
 
 }
